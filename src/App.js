258bb2bc7266845cc4b8b543a7972dc68fb3b605
Jean de la Rosa <jeanluisdelarosa97@gmail.com>
2023-03-14 02:46:39 -0400

    place the title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Let's Play Guess My Number</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
